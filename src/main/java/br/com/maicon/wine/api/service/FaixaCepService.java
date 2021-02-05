13fa6dc19be03c6893e69ee1b442cabdeb560ada
Maicon Araujo de Oliveira <maicon.ao@gmail.com>
2021-02-05 06:50:51 -0300

    Cria classe FaixaCepService

---
+++ src/main/java/br/com/maicon/wine/api/service/FaixaCepService.java
@@ -0,0 +1,25 @@
+package br.com.maicon.wine.api.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.maicon.wine.api.model.FaixaCep;
+import br.com.maicon.wine.api.repository.FaixaCepRepository;
+
+@Service
+public class FaixaCepService {
+	
+	@Autowired
+	private FaixaCepRepository faixaCepRepository;
+	
+	public FaixaCep salvar(FaixaCep faixaCep) {
+		return faixaCepRepository.save(faixaCep);
+	}
+	
+	
+	
+	public void excluir(Long id) {
+		faixaCepRepository.deleteById(id);
+	}
+
+}

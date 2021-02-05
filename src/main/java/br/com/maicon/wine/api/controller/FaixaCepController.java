b454c282d9ad5698cb64211f576388b59a96dd1f
Maicon Araujo de Oliveira <maicon.ao@gmail.com>
2021-02-05 00:42:21 -0300

    Cria classe FaixaCepController

---
+++ src/main/java/br/com/maicon/wine/api/controller/FaixaCepController.java
@@ -0,0 +1,10 @@
+package br.com.maicon.wine.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FaixaCepController {
+
+
+}
--- src/main/resources/db/migration/V001__cria-tabela-cep_loja.sql
+++ src/main/resources/db/migration/V001__cria-tabela-cep_loja.sql
@@ -1,4 +1,4 @@
-create table CEP_LOJA(
+create table FAIXA_CEP(
     ID bigint not null auto_increment,
     CODIGO_LOJA varchar(100) not null,
     FAIXA_INICIO bigint not null,

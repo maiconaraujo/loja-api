8307d3509bcb63d695c2d21291484e9db6e2f111
Maicon Araujo de Oliveira <maicon.ao@gmail.com>
2021-02-04 23:51:35 -0300

    Criar Projeto

---
+++ src/main/java/br/com/maicon/wine/api/WineApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.maicon.wine.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WineApiApplication.class, args);
+	}
+
+}
